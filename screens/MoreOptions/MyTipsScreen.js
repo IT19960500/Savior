8e4920b17b467e48f19eb22d98fe52d44a5666bf
it19951522 <it19951522@my.sliit.lk>
2023-03-26 13:57:45 +0530

    Tips screens and functions added

--- screens/MoreOptions/MyTipsScreen.js
+++ screens/MoreOptions/MyTipsScreen.js
@@ -1,14 +1,187 @@
-import { Text, View } from "react-native";
-import React, { Component } from "react";
+import {
+  View,
+  Text,
+  Button,
+  SafeAreaView,
+  Modal,
+  ActivityIndicator,
+  FlatList,
+  ImageBackground,
+  ScrollView,
+  Pressable
+} from "react-native";
+import AppStyles from "../../styles/AppStyles";
+import { db} from "../../config/firebase";
+import { auth } from "../../config/firebase";
+import React, {useState} from "react";
+import AddTipsModal from "../../components/AddTipsModal";
+import {
+  collection,
+  addDoc,
+  query,
+  where,
+  getDocs,
+  deleteDoc,
+  doc,
+  setDoc,
+} from "firebase/firestore";
+import { Card } from "react-native-elements/dist/card/Card";
+import { FAB } from "react-native-elements";
+import { FormButton } from "../../components";
 
-export class MyTipsScreen extends Component {
-  render() {
+
+export default function MyTipsScreen({ navigation, route }) {
+
+  const [modalVisible, setModalVisible] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [isRefreshing, setIsRefreshing] = React.useState(false);
+  const [tips, setTips] = React.useState([]);
+  const [tipsTitle, setTipsTitle] = useState("");
+  const [tipsDescription, setTipsDescription] = useState("");
+  const [img, setImg] = useState("");
+
+  let showContent = () => {
     return (
       <View>
-        <Text>MyTipsScreen</Text>
+        {isLoading ? <ActivityIndicator size="large" /> : showTipList()}
+        <FAB
+          style={AppStyles.buttons3}
+          title="+"
+          color="red"
+          placement="right"
+          size="large"
+          onPress={() => setModalVisible(true)}
+        />
+
       </View>
     );
-  }
-}
+  };
 
-export default MyTipsScreen;
+  let loadTipsList = async () => {
+    const q = query(
+      collection(db, "tips_list"),
+      where("userId", "==", auth.currentUser.uid)
+    );
+
+    const querySnapshot = await getDocs(q);
+    let tips = [];
+    querySnapshot.forEach((doc) => {
+      let tip = doc.data();
+      tip.id = doc.id;
+      tips.push(tip);
+    });
+
+    setTips(tips);
+    setIsLoading(false);
+    setIsRefreshing(false);
+  };
+
+  if (isLoading) {
+    loadTipsList();
+  }
+
+  let renderTipItem = ({ item }) => {
+    return (
+      <ScrollView>
+        <Card>
+          <View
+            style={[
+              AppStyles.rowContainer,
+              AppStyles.rightMargin,
+              AppStyles.leftMargin,
+            ]}
+          >
+            <View style={AppStyles.fillSpace}>
+              <Text style={AppStyles.renderText}>{item.tips_title}</Text>
+              <Text>{item.tips_description}</Text>
+            </View>
+          </View>
+
+          <FormButton
+            style={AppStyles.buttons1}
+            buttonTitle="Update"
+            onPress={() =>
+              navigation.navigate("UpdateTips", {
+                id: item.id,
+                tips_title: item.tips_title,
+                tips_description: item.tips_description,
+              })
+            }
+          >
+            <Text style={AppStyles.buttonText}>Update</Text>
+          </FormButton>
+          <FormButton
+            style={AppStyles.buttons2}
+            onPress={() => deleteTips(item.id)}
+            buttonTitle="Delete"
+          >
+          </FormButton>
+        </Card>
+      </ScrollView>
+    );
+  };
+
+  let showTipList = () => {
+    return (
+      <FlatList
+        data={tips}
+        refreshing={isRefreshing}
+        onRefresh={() => {
+          loadTipsList();
+          setIsRefreshing(true);
+        }}
+        renderItem={renderTipItem}
+        keyExtractor={(item) => item.id}
+      />
+    );
+  };
+
+  let addTips = async (tipsTitle,tipsDescription) => {
+    let tipsSave = {
+      tips_title: tipsTitle,
+      tips_description: tipsDescription,
+      
+      userId: auth.currentUser.uid,
+    };
+    const docRef = await addDoc(collection(db, "tips_list"), tipsSave);
+
+    tipsSave.id = docRef.id;
+
+    let updatedTips = [...tips];
+    updatedTips.push(tipsSave);
+
+    setTips(updatedTips);
+  };
+
+  let deleteTips = async (tipId) => {
+    await deleteDoc(doc(db, "tips_list", tipId));
+    let updatedTips = [...tips].filter((item) => item.id != tipId);
+    setTips(updatedTips);
+  };
+
+  return (
+    <SafeAreaView>
+      <View
+        style={[
+          AppStyles.rowContainer,
+          AppStyles.rightAligned,
+          AppStyles.rightMargin,
+          AppStyles.topMargin,
+        ]}
+      />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <AddTipsModal
+          onClose={() => setModalVisible(false)}
+          addTips={addTips}
+        />
+      </Modal>
+      <Text style={AppStyles.pageHeader1}>My Tips</Text>
+      {showContent()}
+    </SafeAreaView>
+  );
+}
--- screens/UpdateTips.js
+++ screens/UpdateTips.js
@@ -1,9 +1,60 @@
 import React, { useState } from "react";
-
 import { Text, TextInput, View, StyleSheet } from "react-native";
+import AppStyles from "../styles/AppStyles";
+import { Pressable } from "react-native";
+import { doc, updateDoc, setDoc } from "firebase/firestore";
+import { db } from "../config/firebase";
+import { FormInput } from "../components";
 
 const UpdateTips = ({ route, navigation }) => {
-  return <View style={styles.container}></View>;
+  const {
+    id,
+    tips_title,
+    tips_description,
+  } = route.params;
+
+  console.log(id);
+
+  console.log(tips_title);
+
+  const [tipsTitle, setTipsTitile] = useState("");
+
+  const [tipsDescription, setTipsDescription] = useState("");
+
+  let updateList = () => {
+    const tipsRef = doc(db, "tips_list", id);
+    setDoc(tipsRef, { tips_title: tipsTitle }, { merge: true });
+    setDoc(tipsRef, { tips_description: tipsDescription }, { merge: true });
+  };
+
+  return (
+    <View style={styles.container}>  
+      <FormInput
+        placeholder="Title"
+        value={tipsTitle}
+        onChangeText={setTipsTitile}
+        iconType="edit"
+      />
+      <FormInput
+        placeholder="Description"
+        value={tipsDescription}
+        onChangeText={setTipsDescription}
+        iconType="form"
+      />
+      <Pressable style={AppStyles.buttons2} onPress={() => updateList(id)}>
+        <Text style={AppStyles.buttonText}>Update</Text>
+      </Pressable>
+    </View>
+  );
 };
 
 export default UpdateTips;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
